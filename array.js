8586d518af704347dbf6caef903150917937ae5a
Cedric Lapi <lapicedric@gmail.com>
2024-03-05 23:07:41 +0100

    insertAt function added

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-
+/*
 function pousse(arr, num){
     for(var i = arr.length; i > 0; i--){
         arr[i] = arr[i-1];
@@ -34,6 +34,33 @@ function popfront(arr) {
 
 var array = [5, 7, 2, 3];
 popfront(array);
+
+
+*/
+
+function insertAt(arr, index, val){
+
+    if(index < 0 || index > arr.length){
+        console.log("The index is out of range!");
+        return arr;
+    }
+
+    var arrNew = [];
+
+    for(var i = 0; i < index; i++ ){
+        arrNew.push(arr[i]);
+    }
+
+
+    arrNew.push(val);
+
+    for(var j = index; j < arr.length; j++){
+        arrNew.push(arr[j]);
+    }
+
+    return arrNew;
+}
+
 
 
 
